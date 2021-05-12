f734c0beb4a9610eac2d138dd2b083a79af75672
Diego <diego2003rod@gmail.com>
2021-05-12 08:15:43 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart
    #	pubspec.lock

---
+++ lib/lib/Reminder.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+
+class Reminder extends StatelessWidget {
+  Reminder({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(11.0, 99.0),
+            child: Container(
+              width: 406.0,
+              height: 395.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(53.0),
+                color: const Color(0xff2f52e0),
+                border: Border.all(width: 1.0, color: const Color(0xff707070)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(175.0, 124.0),
+            child: Text(
+              'Food',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 35,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(38.0, 165.0),
+            child: Text(
+              'Meal:',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(38.0, 303.0),
+            child: Text(
+              'Name of the Food:',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(38.0, 234.0),
+            child: Text(
+              'Snack:',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(38.0, 413.0),
+            child: Text.rich(
+              TextSpan(
+                style: TextStyle(
+                  fontFamily: 'Segoe UI',
+                  fontSize: 25,
+                  color: const Color(0xffffffff),
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Date',
+                  ),
+                  TextSpan(
+                    text: ':',
+                    style: TextStyle(
+                      color: const Color(0xff707070),
+                    ),
+                  ),
+                ],
+              ),
+              textHeightBehavior:
+                  TextHeightBehavior(applyHeightToFirstAscent: false),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(225.0, 413.0),
+            child: Text(
+              'Time:',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(11.0, 500.0),
+            child: Container(
+              width: 406.0,
+              height: 223.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(53.0),
+                color: const Color(0xff2f52e0),
+                border: Border.all(width: 1.0, color: const Color(0xff707070)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(165.0, 553.0),
+            child: Text(
+              'Water',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 35,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(19.0, 605.0),
+            child: Text(
+              'When would you like your reminder?',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 24,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(63.0, 653.0),
+            child: Text(
+              'Date:',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(264.0, 653.0),
+            child: Text(
+              'Time:',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(11.0, 731.0),
+            child: Container(
+              width: 406.0,
+              height: 96.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(53.0),
+                color: const Color(0xff2d9bfa),
+                border: Border.all(width: 1.0, color: const Color(0xff707070)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(92.0, 753.0),
+            child: Text(
+              'Save Reminder',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 35,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
